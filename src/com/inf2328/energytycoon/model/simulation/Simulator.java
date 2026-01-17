a28996183666cfedb7e7e08f00fcec1e90fc36ad
TKPinaul <pkt.paulin@gmail.com>
2026-01-17 13:40:35 +0000

    feat: implement text-based online simulation (Simulator)

--- src/com/inf2328/energytycoon/model/simulation/Simulator.java
+++ src/com/inf2328/energytycoon/model/simulation/Simulator.java
@@ -3,18 +3,24 @@ package com.inf2328.energytycoon.model.simulation;
 import com.inf2328.energytycoon.model.city.GameState;
 import com.inf2328.energytycoon.model.player.Player;
 import com.inf2328.energytycoon.model.city.City;
-import com.inf2328.energytycoon.model.building.Residence;
 import com.inf2328.energytycoon.controller.GameController;
 import com.inf2328.energytycoon.model.building.PowerPlant;
 import com.inf2328.energytycoon.model.energy.EnergyType;
 
-// Simulateur de jeu
+/*
+ * Simulateur de jeu
+ * 
+ * @param player : le joueur
+ * @param city : la ville
+ * @param gameState : l'état du jeu
+ * @param controller : le contrôleur
+ */
 public class Simulator {
 
     public static void main(String[] args) {
 
-        // initialisation du joueur avec 1000€
-        Player player = new Player(1000);
+        // initialisation du joueur avec 1200€
+        Player player = new Player(1200);
 
         // Création de la ville
         City city = new City();
@@ -26,17 +32,16 @@ public class Simulator {
         GameController controller = new GameController(gameState);
 
         // Ajout des résidence
-        // Les coûts et niveaux max sont maintenant standardisés
-        controller.buildResidence(new Residence());
-        controller.buildResidence(new Residence());
-        controller.buildResidence(new Residence());
+        controller.buyResidence(1);
+        controller.buyResidence(1);
+        controller.buyResidence(1);
 
         // Test achat direct niveau 2
         controller.buyResidence(2);
 
-        // Ajout des centrales
-        controller.buildPowerPlant(new PowerPlant(EnergyType.SOLAR));
-        controller.buildPowerPlant(new PowerPlant(EnergyType.WIND));
+        // Ajout des centrales via le controleur (methode buy existante)
+        controller.buyPowerPlant(EnergyType.SOLAR);
+        controller.buyPowerPlant(EnergyType.WIND);
 
         // Simulation du jeu
         while (!controller.isGameOver()) {

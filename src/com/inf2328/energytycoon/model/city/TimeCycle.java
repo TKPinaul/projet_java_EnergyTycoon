cd7093a0662652b5196bdbf336165374f6dc9322
BOTRE LARE Aboudou <botrelareaboudo@gmail.com>
2026-01-17 12:55:35 +0000

    application des regles de jeux de gestion de la ville et du temps

--- src/com/inf2328/energytycoon/model/city/TimeCycle.java
+++ src/com/inf2328/energytycoon/model/city/TimeCycle.java
@@ -1,6 +1,5 @@
 package com.inf2328.energytycoon.model.city;
 
-
 /*
  * Classe représentant le cycle de temps
  * 
@@ -17,31 +16,46 @@ public class TimeCycle {
         EVENING,
         NIGHT,
     }
-    
+
+    private int hour;
     private int day;
     private int week;
     private int month;
-    private TimeOfDay timeOfDay;
 
     // Constructeur
     public TimeCycle() {
+        hour = 8; // On commence à 8h le matin
         day = 1;
         week = 1;
         month = 1;
-        timeOfDay = TimeOfDay.DAY;
     }
 
     // Méthode de passage à la prochaine horraire
     public void nextTimeSlot() {
-        switch (timeOfDay) {
-            case MORNING -> timeOfDay = TimeOfDay.DAY;
-            case DAY -> timeOfDay = TimeOfDay.EVENING;
-            case EVENING -> timeOfDay = TimeOfDay.NIGHT;
-            case NIGHT -> {
-                timeOfDay = TimeOfDay.MORNING;
-                nextDay();
-            }
+        hour++;
+        if (hour >= 24) {
+            hour = 0;
+            nextDay();
         }
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public String getFormattedTime() {
+        return String.format("%02dh00", hour);
+    }
+
+    // Récupération de l'heure de la journée basée sur l'heure numérique
+    public TimeOfDay getTimeOfDay() {
+        if (hour >= 6 && hour < 12)
+            return TimeOfDay.MORNING;
+        if (hour >= 12 && hour < 18)
+            return TimeOfDay.DAY;
+        if (hour >= 18 && hour < 24)
+            return TimeOfDay.EVENING;
+        return TimeOfDay.NIGHT;
     }
 
     // Méthode de passage à la journée suivante
@@ -56,11 +70,6 @@ public class TimeCycle {
             week = 1;
             month++;
         }
-    }
-
-    // Récupération de l'heure de la journée
-    public TimeOfDay getTimeOfDay() {
-        return timeOfDay;
     }
 
     // Récupération du jour
@@ -78,9 +87,21 @@ public class TimeCycle {
         return month;
     }
 
+    public int getTotalDays() {
+        return (month - 1) * 28 + (week - 1) * 7 + (day - 1);
+    }
+
+    public void restart() {
+        hour = 8;
+        day = 1;
+        week = 1;
+        month = 1;
+    }
+
     // Représentation du cycle de temps
     @Override
     public String toString() {
-        return String.format("Day %d, Week %d, Month %d, Time: %s", day, week, month, timeOfDay);
+        return String.format("Month %d, Week %d, Day %d, Time: %s (%s)",
+                month, week, day, getFormattedTime(), getTimeOfDay());
     }
 }

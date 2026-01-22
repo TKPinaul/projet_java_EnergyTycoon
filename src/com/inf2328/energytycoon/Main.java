fe1fea6c0c6e60f7b0c4c765d665c951854bc192
Paulin Tepe <125353528+TKPinaul@users.noreply.github.com>
2026-01-22 13:53:30 +0000

    correection main
    Remplacement du main java

--- src/com/inf2328/energytycoon/Main.java
+++ src/com/inf2328/energytycoon/Main.java
@@ -1,7 +1,30 @@
 package com.inf2328.energytycoon;
 
+import javax.swing.SwingUtilities;
+import com.inf2328.energytycoon.controller.GameController;
+import com.inf2328.energytycoon.model.city.City;
+import com.inf2328.energytycoon.model.city.GameState;
+import com.inf2328.energytycoon.model.player.Player;
+import com.inf2328.energytycoon.view.MainFrame;
+
+/*
+ * Point d'entrée du jeu
+ */
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Initialisation des données du jeu
+        Player player = new Player(1200);
+        City city = new City();
+        GameState gameState = new GameState(player, city);
+        GameController controller = new GameController(gameState);
+
+        // Initialisation du jeu
+        controller.restartGame();
+
+        // Lancement de l'interface graphique
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame(controller);
+            frame.setVisible(true);
+        });
     }
 }
